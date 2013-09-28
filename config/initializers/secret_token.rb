e29181c29c388426c39522ae930d0ee49da5743f
Abhishek Gadiraju <abhishek.gadiraju@gmail.com>
2013-09-28 17:07:27 -0400

    initial commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RelationshipTracker::Application.config.secret_key_base = '6372eb0eda9a1adcdee3909f78a485822786fd292ce32b9332c909c78d0faa15aa30f0c327bf8a6c461beed01503f7c1ab0cb6daa457cb4e28e8e63add3db5a7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RelationshipTracker::Application.config.session_store :cookie_store, key: '_relationship_tracker_session'
